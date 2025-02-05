a8e8adf5ecb972c683b54cb2dae7132c1e274927
Angel Mauricio <168794154+El-Rincon-Del-Cine@users.noreply.github.com>
2025-02-05 17:38:18 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,51 +1,49 @@
-self.addEventListener('install', event => {
-    console.log('Service Worker: Instalando...');
+self.addEventListener('install', (event) => {
+    console.log('Service Worker: Instalado');
     event.waitUntil(
         caches.open('mi-cache-v1').then(cache => {
-            return Promise.all([
-                    '/',
-                    '/index.html',
-                    '/Contactanos.html',
-                    '/Oferta educativa.html',
-                    '/plan.html',
-                    '/Ubicacion.html',
-                    '/estilos.css',
-                    '/plan.css',
-                    '/ofertaE.css',
-                    '/app.js',
-                    'imagenes/1.jpg',
-                    'imagenes/2.jpg',
-                    'imagenes/3.jpg',
-                    'imagenes/4.jpg',
-                    'imagenes/5.jpg',
-                    'imagenes/profesional.jpg',
-                    'imagenes/Software.jpg',
-                    'imagenes/tla.jpg',
-                    'imagenes/unam.jpg',
-                    'imagenes/valor.png',
-                    'imagenes/actitud.png',
-                    'imagenes/beca.png',
-                    'imagenes/benemerita.png',
-                    'imagenes/conocimiento.png',
-                    'imagenes/escudo.png',
-                    'imagenes/escuelassuperior.png',
-                    'imagenes/graduacion.png',
-                    'imagenes/icon.png',
-                    'imagenes/icono1.png',
-                    'imagenes/icono2.png',
-                    'imagenes/inicio_cap.png',
-                    'imagenes/itson.png',
-                    'imagenes/logo_unam.png',
-                    'imagenes/mujer-removebg-preview.png',
-                    'imagenes/multitalentoso.png',
-                    'imagenes/papeleria.png',
-                    'imagenes/par_students_removebg_preview.png',
-                    'imagenes/plan_cap.png',
-                    'imagenes/planeta-tierra.png',
-                    'imagenes/public-service.png',
-                    '/offline.html',
-                    '/manifest.json'
-                ].map(url =>
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/estilos.css',
+                '/manifest.json',
+                '/Oferta educativa.html',
+                '/ofertaE.css',
+                '/plan.css',
+                '/plan.html',
+                '/Ubicacion.html',
+                '/Contactanos.html',
+                '/imagenes/1.jpg',
+                '/imagenes/2.jpg',
+                '/imagenes/3.jpg',
+                '/imagenes/4.jpg',
+                '/imagenes/5.jpg',
+                '/imagenes/actitud.png',
+                '/imagenes/beca.png',
+                '/imagenes/benemerita.png',
+                '/imagenes/conocimiento.png',
+                '/imagenes/escudo.png',
+                '/imagenes/escuelasuperior.png',
+                '/imagenes/graduacion.png',
+                '/imagenes/icon.png',
+                '/imagenes/icono1.png',
+                '/imagenes/icono2.png',
+                '/imagenes/inicio_cap.png',
+                '/imagenes/itson.png',
+                '/imagenes/logo_unam.png',
+                '/imagenes/mujer-removebg-preview.png',
+                '/imagenes/multitalentoso.png',
+                '/imagenes/papeleria.png',
+                '/imagenes/par_students-removebg-preview.png',
+                '/imagenes/plan_cap.png',
+                '/imagenes/planeta-tierra.png',
+                '/imagenes/profesional.jpg',
+                '/imagenes/public-service.png',
+                '/imagenes/Software.jpg',
+                '/imagenes/tia.jpg',
+                '/imagenes/unam.jpg',
+                '/imagenes/valor.png'
+            ].map(url =>
                     fetch(url) // Intentamos obtener cada archivo
                         .then(response => {
                             if (!response.ok) throw new Error(`Error al cargar ${url}`);
@@ -68,6 +66,7 @@ self.addEventListener('activate', event => {
             return Promise.all(
                 cacheNames.map(cacheName => {
                     if(cacheWhitelist.indexOf(cacheName) === -1){
+                        console.log('Limpiando cache antigua');
                         return caches.delete(cacheName);
                     }
                 })
@@ -77,21 +76,12 @@ self.addEventListener('activate', event => {
     return self.clients.claim();
 });     
 
-self.addEventListener('fetch', event => {
-    console.log('Service Worker: fetch', event.request.url);
+self.addEventListener('fetch', (event) => {
+    console.log('Service Worker: Fetching', event.request.url);
     event.respondWith(
-        caches.match(event.request).then(response => {
-            if (response) {
-                return response;
-            }
-
-            return fetch(event.request).then(networkResponse => {
-                return networkResponse;
-            }).catch(() => {
-                if (event.request.mode === 'navigate') {
-                    return caches.match('/offline.html');
-                }
-            });
-        })
+        caches.match(event.request)
+        .then(response => {
+            return response || fetch(event.request);
+        }).catch(() =>caches.match('/offline.html'))
     );
 });
